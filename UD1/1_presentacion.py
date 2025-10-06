2e5eee9c9124b4e8f6f189254f62e6923fbaa089
roddyvidal <vidalsanchezroddy7@gmail.com>
2025-10-06 14:21:58 +0200

    add

--- UD1/1_presentacion.py
+++ UD1/1_presentacion.py
@@ -1,4 +1,6 @@
 nombre= input("¿Cómo te llamas? ")
 edad= input("¿Cual es tu edad? ")
+estudias = input("¿Estudias? (Y/N): ")
 
-print(f"Tu nombre es {nombre}, y tienes {edad}años ")
+
+print(f"Tu nombre es {nombre}, y tienes {edad}años , y ¿estudio? {estudias}")
---
+++ b1_10_login.py
@@ -0,0 +1,12 @@
+
+lista_usuarios = []
+
+opcion = input("Bienvenido al menu principal de roddy.com\n"
+               "A) Registrar\n"
+               "B) Iniciar sesion\n"
+               "C) Salir\n")
+
+if opcion == "A":
+    while lista_usuarios != input("Introduce tu nombre de usuario"):
+    contraseña = input("Introduce tu contraseña: ")
+if opcion == "A":
