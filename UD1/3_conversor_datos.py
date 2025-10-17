fdfa9ef0a813b950bcc2a3d152d8dd80a1c04204
roddyvidal <vidalsanchezroddy7@gmail.com>
2025-10-17 13:26:02 +0200

    add

--- UD1/3_conversor_datos.py
+++
@@ -1,6 +0,0 @@
-name=(input("¿Cómo te llamas?: "))
-age=int(input("¿Qué edad tienes: "))
-height=float(input("¿Cuanto mides?"))
-
-print(f"Hola me llamo {name}, Tengo {age} años y mido {height}m")
-
